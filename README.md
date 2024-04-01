afb6218d388d62a1dbf6d9774ba5e70f74ff5813
Lee Jimin <etote306@gmail.com>
2024-04-02 00:24:50 +0900

    map test
    map test

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ precedent
 
 npm i -D prisma
 
+npm install @picocss/pico
+@use "pico";
+
+npm install --save-dev sass
--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,14 +1,19 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Map from '@/app/ui/map';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+      />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row"></div>
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
@@ -27,17 +32,8 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
-        <div id="map" style="width:500px;height:400px;"></div>
-          <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."></script>
-          <script>
-            var container = document.getElementById('map');
-            var options = {
-              center: new kakao.maps.LatLng(33.450701, 126.570667),
-              level: 3
-            };
-
-            var map = new kakao.maps.Map(container, options);
-          </script>
+        <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+          <Map />
       </div>
     </main>
   );
