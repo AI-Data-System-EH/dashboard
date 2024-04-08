2d1f5c661a92c76db24c8ab3db5e7ede781487f9
Ruzzy77 <hiyaq77@gmail.com>
2024-04-08 20:26:26 +0900

    Cleanup repo with Next.js / Spline

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+    return (
+        <div>
+            <Spline scene="https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode" />
+        </div>
+    );
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": "postcss-nesting",
-    tailwindcss: { config: "./tailwind.config.js" },
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
